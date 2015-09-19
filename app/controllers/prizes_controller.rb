e24bddbd6e9032c4d22922f50569e3b0ff0c64f9
Federico <fedelc.us@gmail.com>
2015-09-19 03:22:09 -0300

    Prizes

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -1,9 +1,15 @@
 class PrizesController < InheritedResources::Base
 
-  private
+  def index
+    @prizes = Prize.all
+  end
 
+  private
+    def prize
+      @prize ||= Prize.find(params[:id])
     def prize_params
-      params.require(:prize).permit()
+      params.require(:prize).permit(:name, :description, :bottles, )
     end
 end
+
 
