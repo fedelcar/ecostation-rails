1ab499485a9cd1e450f13064e91e9de007d93f0d
Federico López Cañás <federico.lopez@wolox.com.ar>
2015-10-05 14:39:56 -0300

    Disable show in app in rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,34 +1,20 @@
 RailsAdmin.config do |config|
-
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-
   config.actions do
+    # root actions
     dashboard                     # mandatory
+    # collection actions
     index                         # mandatory
     new
     export
+    history_index
     bulk_delete
+    # member actions
     show
     edit
     delete
-    show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+    history_show
+    # show_in_app do
+    #   except ['Company', 'Prize']
+    # end
   end
 end
