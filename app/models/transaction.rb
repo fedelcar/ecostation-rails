12e36e3691d66df5dc4222db5d8cf8448d011d07
fedelcar <federico.lopez@wolox.com.ar>
2015-11-01 11:49:57 -0300

    Validates that company owns station

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,18 @@ class Transaction < ActiveRecord::Base
   belongs_to :station
 
   validates :user,:station, presence: true
+  validate :company_owns_station
   after_create :update_user_bottle_count
+
+  def company_owns_station
+    if self.user.present? and self.station.present?
+      company = self.user.company
+      station = self.station
+      if station.company != company
+        errors.add(:station, "Esta estación no es de esta empresa.")
+      end
+    end
+  end
 
   def update_user_bottle_count
     user = User.find(self.user)
