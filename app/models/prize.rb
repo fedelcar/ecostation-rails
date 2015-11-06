e0d4b822a900af904c56a6142a8c6bc09afbc382
fedelcar <federico.lopez@wolox.com.ar>
2015-11-05 23:31:26 -0300

    prize company validation

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -7,4 +7,16 @@ class Prize < ActiveRecord::Base
   validates :bottles, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   scope :own, -> { where("company_id=?", User.current.company_id) unless User.current.site_admin }
+  validate :user_belongs_to_company
+
+  def company_owns_station
+    if self.company.present? and User.current.present?
+      company = self.company
+      station = User.current.company
+      if station.company != company
+        errors.add(:company, "No puede asignarle premios a otra empresa.")
+      end
+    end
+  end
+
 end
