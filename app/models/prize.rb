05d262ad7b348bc1fd93bd2a73d5826239c43007
fedelcar <federico.lopez@wolox.com.ar>
2015-11-06 00:22:19 -0300

    bottles valid" "

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -3,7 +3,7 @@ class Prize < ActiveRecord::Base
   belongs_to :user
   belongs_to :trade
 
-  validates :bottles, :company, :name, :description, presence: true
+  validates :company, :name, :description, presence: true
   validates :bottles, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
   scope :own, -> { where("company_id=?", User.current.company_id) unless User.current.site_admin }
