62f0aeb4aa20330cf2c62522d8d6092190f11d06
Federico López Cañás <fedelc.us@gmail.com>
2016-05-28 22:00:12 -0300

    Bottles next to button

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -3,8 +3,6 @@ class StaticsController < ApplicationController
   def home
     if current_user.present?
       @bottles = current_user.bottles
-      @transactions = current_user.transactions.includes(:station).order(created_at: :desc).last(5)
-      @trades = current_user.trades.order(created_at: :desc).last(5)
       if current_user.site_admin?
         @stations = Station.all
       else
